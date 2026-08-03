31088d64ccd6488bdd67cade2341b7d055b67499
Hexel-spec <hryxu@ucdavis.edu>
2026-08-03 14:21:48 -0700

    abw and mindscape added

--- script.js
+++ script.js
@@ -4,11 +4,23 @@
 
     const myWork = document.querySelector('#work');
     const aboutMe = document.querySelector('#about');
-    const contact = document.querySelector('#contact')
+    const contact = document.querySelector('#contact');
+    const pixel = document.querySelector('#pixel');
+    const abw = document.querySelector('#abw');
+    const bittersweet = document.querySelector('#bittersweet');
+    const mindscape = document.querySelector('#mindscape');
+    const axiable = document.querySelector('#axiable');
 
     const workLink = document.querySelector('#work-link');
     const aboutLink = document.querySelector('#about-link');
     const contactLink = document.querySelector('#contact-link');
+    const pixelCover = document.querySelector('#pixel-cover');
+    const abwCover = document.querySelector('#abw-cover');
+    const bittersweetCover = document.querySelector('#bittersweet-cover');
+    const mindscapeCover = document.querySelector('#mindscape-cover');
+    const axiableCover = document.querySelector('#axiable-cover');
+
+    const backBtns = document.querySelectorAll('.back');
 
     workLink.addEventListener('click', function () {
         if (myWork.classList.contains('hidden')) {
@@ -27,6 +39,32 @@
             changePage(contactLink, contact);
         }
     })
+
+    pixelCover.addEventListener('click', function(){
+        changePage(pixelCover, pixel);
+    })
+
+    abwCover.addEventListener('click', function(){
+        changePage(abwCover, abw);
+    })
+
+    bittersweetCover.addEventListener('click', function(){
+        changePage(bittersweetCover, bittersweet);
+    })
+    
+    mindscapeCover.addEventListener('click', function(){
+        changePage(mindscapeCover, mindscape);
+    })
+
+    mindscapeCover.addEventListener('click', function(){
+        changePage(mindscapeCover, mindscape);
+    })
+
+    backBtns.forEach(element => {
+        element.addEventListener('click', function(){
+            changePage(workLink, myWork);
+        })
+    });
     
 
     // function changePage(link, page) {
