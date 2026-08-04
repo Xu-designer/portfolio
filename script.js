716a162d09402605ca353a7dd517b41650fdd65d
Hexel-spec <hryxu@ucdavis.edu>
2026-08-03 17:00:14 -0700

    bittersweet added, aos behavior

--- script.js
+++ script.js
@@ -20,60 +20,116 @@
     const mindscapeCover = document.querySelector('#mindscape-cover');
     const axiableCover = document.querySelector('#axiable-cover');
 
-    const backBtns = document.querySelectorAll('.back');
+    // const backBtns = document.querySelectorAll('.back');
+    const backBtn = document.querySelector('.back');
+
+    if ('history' in window) {
+        window.history.scrollRestoration = 'manual';
+    }
 
     workLink.addEventListener('click', function () {
         if (myWork.classList.contains('hidden')) {
             changePage(workLink, myWork);
+            window.scrollTo({
+                top: 300,
+                behavior: 'smooth'
+            });
+            backBtn.classList.add('hidden');
         }
     })
 
     aboutLink.addEventListener('click', function () {
         if (aboutMe.classList.contains('hidden')) {
             changePage(aboutLink, aboutMe);
+            window.scrollTo({
+                top: 300,
+                behavior: 'smooth'
+            });
+            backBtn.classList.add('hidden');
         }
     })
 
     contactLink.addEventListener('click', function () {
         if (contact.classList.contains('hidden')) {
             changePage(contactLink, contact);
+            window.scrollTo({
+                top: 300,
+                behavior: 'smooth'
+            });
+            backBtn.classList.add('hidden');
         }
     })
 
-    pixelCover.addEventListener('click', function(){
+    pixelCover.addEventListener('click', function () {
         changePage(pixelCover, pixel);
+        backBtn.classList.remove('hidden');
+        window.scrollTo({
+            top: 300,
+            behavior: 'smooth'
+        });
     })
 
-    abwCover.addEventListener('click', function(){
+    abwCover.addEventListener('click', function () {
         changePage(abwCover, abw);
+        backBtn.classList.remove('hidden');
+        window.scrollTo({
+            top: 300,
+            behavior: 'smooth'
+        });
     })
 
-    bittersweetCover.addEventListener('click', function(){
+    bittersweetCover.addEventListener('click', function () {
         changePage(bittersweetCover, bittersweet);
+        backBtn.classList.remove('hidden');
+        window.scrollTo({
+            top: 300,
+            behavior: 'smooth'
+        });
     })
-    
-    mindscapeCover.addEventListener('click', function(){
+
+    mindscapeCover.addEventListener('click', function () {
         changePage(mindscapeCover, mindscape);
+        backBtn.classList.remove('hidden');
+        window.scrollTo({
+            top: 300,
+            behavior: 'smooth'
+        });
     })
 
-    mindscapeCover.addEventListener('click', function(){
+    mindscapeCover.addEventListener('click', function () {
         changePage(mindscapeCover, mindscape);
+        backBtn.classList.remove('hidden');
+        window.scrollTo({
+            top: 300,
+            behavior: 'smooth'
+        });
     })
 
-    backBtns.forEach(element => {
-        element.addEventListener('click', function(){
-            changePage(workLink, myWork);
-        })
-    });
-    
+    backBtn.addEventListener('click', function () {
+        changePage(workLink, myWork);
+        backBtn.classList.add('hidden');
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    })
 
+    // backBtns.forEach(element => {
+    //     element.addEventListener('click', function(){
+    //         changePage(workLink, myWork);
+    //     })
+    // });
+
+    // Regular change page
     // function changePage(link, page) {
     //     document.querySelector('.showing').className = 'hidden';
     //     document.querySelector('.selected').className = 'unselected';
     //     page.className = 'showing'
     //     link.className = 'selected';
     // }
-    
+
+
+    // Fade in/out functionality
     function changePage(link, page) {
         fadeOut(document.querySelector('.showing'));
         document.querySelector('.selected').className = 'unselected';
