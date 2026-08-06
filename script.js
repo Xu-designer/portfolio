3f5199b3d93ccffdcd02cd6f21f00f8b3b8e6607
Hexel-spec <hryxu@ucdavis.edu>
2026-08-05 20:47:16 -0700

    teal style

--- script.js
+++ script.js
@@ -31,7 +31,7 @@
         if (myWork.classList.contains('hidden')) {
             changePage(workLink, myWork);
             window.scrollTo({
-                top: 300,
+                top: 600,
                 behavior: 'smooth'
             });
             backBtn.classList.add('hidden');
@@ -42,7 +42,7 @@
         if (aboutMe.classList.contains('hidden')) {
             changePage(aboutLink, aboutMe);
             window.scrollTo({
-                top: 300,
+                top: 600,
                 behavior: 'smooth'
             });
             backBtn.classList.add('hidden');
@@ -53,7 +53,7 @@
         if (contact.classList.contains('hidden')) {
             changePage(contactLink, contact);
             window.scrollTo({
-                top: 300,
+                top: 600,
                 behavior: 'smooth'
             });
             backBtn.classList.add('hidden');
@@ -64,7 +64,7 @@
         changePage(pixelCover, pixel);
         backBtn.classList.remove('hidden');
         window.scrollTo({
-            top: 300,
+            top: 600,
             behavior: 'smooth'
         });
     })
@@ -73,7 +73,7 @@
         changePage(abwCover, abw);
         backBtn.classList.remove('hidden');
         window.scrollTo({
-            top: 300,
+            top: 600,
             behavior: 'smooth'
         });
     })
@@ -82,7 +82,7 @@
         changePage(bittersweetCover, bittersweet);
         backBtn.classList.remove('hidden');
         window.scrollTo({
-            top: 300,
+            top: 600,
             behavior: 'smooth'
         });
     })
@@ -91,7 +91,7 @@
         changePage(mindscapeCover, mindscape);
         backBtn.classList.remove('hidden');
         window.scrollTo({
-            top: 300,
+            top: 600,
             behavior: 'smooth'
         });
     })
@@ -100,7 +100,7 @@
         changePage(mindscapeCover, mindscape);
         backBtn.classList.remove('hidden');
         window.scrollTo({
-            top: 300,
+            top: 600,
             behavior: 'smooth'
         });
     })
@@ -109,7 +109,7 @@
         changePage(axiableCover, axiable);
         backBtn.classList.remove('hidden');
         window.scrollTo({
-            top: 300,
+            top: 600,
             behavior: 'smooth'
         });
     })
@@ -118,7 +118,7 @@
         changePage(workLink, myWork);
         backBtn.classList.add('hidden');
         window.scrollTo({
-            top: 0,
+            top: 600,
             behavior: 'smooth'
         });
     })
@@ -167,5 +167,7 @@
         //     element.className = 'showing';
         // }, 300)
     }
+    
+    particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":3.1508467900748314,"direction":"top","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
 
 })()
